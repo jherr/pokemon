6e0753b1c294928a44def317f2104e6b64917335
Jack Herrington <jherr@pobox.com>
2022-02-26 07:13:27 -0800

    Big update

--- scripts/build.js
+++ scripts/build.js
@@ -1,8 +1,29 @@
 const fs = require("fs");
 
-for (const pokemon of JSON.parse(fs.readFileSync("./index.json"))) {
+const index = [];
+for (const pokemon of JSON.parse(fs.readFileSync("./original.json"))) {
+  const image = `images/${pokemon.name.english
+    .toLowerCase()
+    .replace(/\s+/, "-")}.jpg`;
+  if (!fs.existsSync(image)) {
+    console.log(`No image on ${pokemon.name.english}`);
+  }
+  index.push({
+    id: pokemon.id,
+    name: pokemon.name.english,
+    image,
+  });
   fs.writeFileSync(
     `./pokemon/${pokemon.id}.json`,
-    JSON.stringify(pokemon, null, 2)
+    JSON.stringify(
+      {
+        ...pokemon,
+        image,
+      },
+      null,
+      2
+    )
   );
 }
+
+fs.writeFileSync(`./index.json`, JSON.stringify(index, null, 2));
